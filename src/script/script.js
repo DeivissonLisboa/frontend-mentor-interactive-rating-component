ade5e7b62fb7cc7053662d635db13baffb132be9
Deivisson Lisboa <dvilaflor@gmail.com>
2023-02-10 01:05:51 -0300

    Display user selected rating

--- src/script/script.js
+++ src/script/script.js
@@ -1,4 +1,8 @@
 const RATINGS = document.querySelectorAll(".ratings button")
+const RATING_CARD = document.getElementById("rating")
+const THANKS_CARD = document.getElementById("thanksMessage")
+const USER_RATING_SPAN = document.getElementById("userRating")
+const SUBMIT_BUTTON = document.querySelector(".submit-btn")
 let selectedRating
 
 RATINGS.forEach((rating) => {
@@ -8,4 +12,10 @@ RATINGS.forEach((rating) => {
     rating.dataset.status = "selected"
     selectedRating = rating.innerText
   })
+})
+
+SUBMIT_BUTTON.addEventListener("click", () => {
+  RATING_CARD.dataset.status = "hidden"
+  THANKS_CARD.dataset.status = ""
+  USER_RATING_SPAN.innerHTML = selectedRating
 })
